51691e7e4a86c730576c4f886ee9b93d9863ca95
edgarUser <egdar.lopez.isc@gmail.com>
2021-01-29 06:30:43 -0600

    Was enable basic rest configuration

---
+++ src/main/java/com/mx/mservice/controller/WebController.java
@@ -0,0 +1,19 @@
+package com.mx.mservice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mx.mservice.models.SampleResponse;
+
+@RestController
+public class WebController {
+
+	@RequestMapping("/sample")
+	public SampleResponse Sample(@RequestParam(value = "name", defaultValue = "Robot") String name) {
+		SampleResponse response = new SampleResponse();
+		response.setId(1);
+		response.setMessage("Your name is " + name);
+		return response;
+	}
+}
